28fd5e0efcdad5d6489c84ff2701f79040dbc28c
gulnabi-afridi <mac@macs-MacBook-Pro.local>
2023-01-10 14:24:07 +0500

    added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,23 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        <title>Fat Ape Club</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </>
   );
 }
 
